883571a68ec016e8629702f910079a34df651fb4
saianirudh kantabathina <saianirudhster@gmail.com>
2019-02-16 10:21:57 -0800

    text change in test

--- src/test/scala/com/wellpay/OrchestratorSpec.scala
+++ src/test/scala/com/wellpay/OrchestratorSpec.scala
@@ -4,7 +4,7 @@ import org.scalatest._
 class OrchestratorSpec extends FlatSpec {
 
 val orchestrator = new Orchestrator
-  "orchestrator" should "always return a string" in {
+  "orchestrate method" should "return a string" in {
     orchestrator.orchestrate() match {
       case Some(x:String) => assert(x.length > 0)
       case _ => assert(false)
